34fb7a2e0efc97144991e73f0248b4090ee6fc09
Ajay Chaudharyyy <ajayawana4308@gmail.com>
2025-07-03 14:09:21 +0530

    Fix: updated database.js for Render env variable

--- Backend/Config/database.js
+++ Backend/Config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect("mongodb+srv://ajaychaudharyy4308:TFwiMmgrFFR2zmRm@cluster0.nlblak6.mongodb.net/collabDB?retryWrites=true&w=majority");
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error);
